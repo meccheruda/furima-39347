1c9e73ec1387eb50238083cf08177430b96b1c3b
meccheruda <masanao_f@yahoo.co.jp>
2023-05-25 22:52:14 +0900

    バリデーションとログイン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+         validates :nickname, :birth_date, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: "is invalid. Include both letters and numbers" }
+         validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Input full-width characters" }
+         validates :last_name_kana, :first_name_kana,presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: " Input full-width katakana characters" }
 end
