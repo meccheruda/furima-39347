fd8251b6f7c3615d9b59bf84c10f5bcacdbd69b9
meccheruda <masanao_f@yahoo.co.jp>
2023-05-26 22:35:36 +0900

    アソシエーションとバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
 
+  belongs_to :user
   has_one_attached :image
+
+  validates :item_name, :description, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :postage_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank"} 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :delivery
 end
