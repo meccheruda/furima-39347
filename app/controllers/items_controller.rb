18388166e093707e7af12c9776f45bf88f525463
meccheruda <masanao_f@yahoo.co.jp>
2023-05-29 17:34:59 +0900

    URL欄に出品ページへのパスを入力しても飛べないように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,10 @@ class ItemsController < ApplicationController
   end
 
   def new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+      return
+    end
     @item = Item.new
   end
 
