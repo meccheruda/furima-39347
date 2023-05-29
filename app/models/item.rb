bd787df3f240823781a8650f113589055a3e5ac1
meccheruda <masanao_f@yahoo.co.jp>
2023-05-29 17:48:56 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,8 @@ class Item < ApplicationRecord
   validates :postage_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :price, format: { with: /\A[0-9]+\z/, message: 'should be a valid number' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+     only_integer: true, message: 'should be a valid number' }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
