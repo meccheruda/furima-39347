92f6cbdd014532a1504443a38211ccb5e1886d1a
meccheruda <masanao_f@yahoo.co.jp>
2023-05-29 14:21:06 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :item_name, :description, presence: true
+  validates :item_name, :description, :image, :price , presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank"} 
   validates :postage_id, numericality: { other_than: 1, message: "can't be blank"} 
