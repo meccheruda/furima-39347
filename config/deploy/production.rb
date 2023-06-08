d8c07098fcd93b7f4a77f7fd41ebf450cf5bcf55
meccheruda <masanao_f@yahoo.co.jp>
2023-06-08 11:04:22 +0900

    移動

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,6 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '52.195.88.227', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
@@ -59,3 +58,4 @@ server '52.195.88.227', user: 'ec2-user', roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '52.195.88.227', user: 'ec2-user', roles: %w{app db web}
